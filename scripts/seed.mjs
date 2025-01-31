77e21f078f7ab436b038867db60ec209add17fc4
shoib akhtar <shoib2738248@gmail.com>
2025-02-01 02:11:33 +0500

    minnor

--- scripts/seed.mjs
+++ scripts/seed.mjs
@@ -16,7 +16,7 @@ const doctors = [
     specialty: 'Orthopedics',
     city: 'Los Angeles',
     photo: 'https://images.unsplash.com/photo-1612349317150-e413f6a5b16d?w=500&auto=format&fit=crop',
-    description: 'Specialist in neurological disorders with a focus on research and innovative treatment methodologies.',
+    description: 'Expert in orthopedic surgery, specializing in joint replacement, sports injuries, and innovative treatment methodologies.',
     officeLocation: '456 Wellness Blvd, Los Angeles, CA',
     contactInfo: {
       phone: '987-654-3210',
@@ -28,7 +28,7 @@ const doctors = [
     specialty: 'Dermatology',
     city: 'New York',
     photo: 'https://images.unsplash.com/photo-1622253692010-333f2da6031d?q=80&w=1528&auto=format&fit=crop',
-    description: 'Dedicated dermatologist specializing in cosmetic and clinical dermatology, committed to enhancing skin health.',
+    description: 'Dedicated dermatologist specializing in cosmetic and clinical dermatology, committed to enhancing skin health through personalized treatments.',
     officeLocation: '789 Beauty Ave, New York, IL',
     contactInfo: {
       phone: '555-123-4567',
@@ -40,7 +40,7 @@ const doctors = [
     specialty: 'Cardiology',
     city: 'Los Angeles',
     photo: 'https://images.unsplash.com/photo-1651008376811-b90baee60c1f?q=80&w=1374&auto=format&fit=crop',
-    description: 'Compassionate pediatrician with a focus on child health, growth, and preventive care for young patients.',
+    description: 'Compassionate cardiologist with a focus on patient-centered care, specializing in heart disease prevention and advanced treatment.',
     officeLocation: '101 KidCare St, Los Angeles, TX',
     contactInfo: {
       phone: '777-888-9999',
@@ -52,7 +52,7 @@ const doctors = [
     specialty: 'Neurology',
     city: 'San Francisco',
     photo: 'https://images.unsplash.com/photo-1537368910025-700350fe46c7?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    description: 'Experienced ophthalmologist treating eye conditions and specializing in vision correction procedures.',
+    description: 'Renowned neurologist with expertise in diagnosing and treating complex neurological disorders, with a focus on innovative therapies.',
     officeLocation: '321 Vision Way, San Francisco, CA',
     contactInfo: {
       phone: '888-555-1234',
@@ -64,28 +64,28 @@ const doctors = [
     specialty: 'Neurology',
     city: 'San Francisco',
     photo: 'https://plus.unsplash.com/premium_photo-1661580574627-9211124e5c3f?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    description: 'Leading orthopedic surgeon with expertise in sports injuries and joint replacement surgeries.',
+    description: 'Leading orthopedic surgeon with expertise in sports injuries, trauma recovery, and joint replacement surgeries, utilizing the latest medical advancements.',
     officeLocation: '567 BoneCare Rd, San Francisco, FL',
     contactInfo: {
       phone: '444-222-1111',
       email: 'daniel.white@example.com',
     },
   },
-
   {
     name: 'Dr. William Green',
     specialty: 'Orthopedics',
     city: 'Denver',
     photo: 'https://plus.unsplash.com/premium_photo-1681996484614-6afde0d53071?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    description: 'Board-certified psychiatrist with expertise in mental health, therapy, and medication management.',
+    description: 'Board-certified psychiatrist specializing in mental health, therapy, and medication management, committed to holistic patient care.',
     officeLocation: '234 MindCare Ave, Denver, CO',
     contactInfo: {
       phone: '555-987-6543',
       email: 'william.green@example.com',
     },
   },
-
 ];
+
+
 
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';

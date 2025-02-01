3c574a0a69227f2e4c56163684ee28ac76a765b1
shoib akhtar <shoib2738248@gmail.com>
2025-02-01 16:16:50 +0500

    add env

--- src/app/doctors/[id]/page.js
+++ src/app/doctors/[id]/page.js
@@ -1,7 +1,7 @@
 
 import dbConnect from "../../../../utils/dbConnect.mjs";
 import Doctor from "../../../../models/Doctor.mjs";
-
+import Link from "next/link";
 const DoctorDetails = async ({ params }) => {
   const { id } = await params
 
@@ -24,6 +24,22 @@ const DoctorDetails = async ({ params }) => {
 
   return (
     <div className="container mx-auto p-4">
+       <div className="container mx-auto p-4">
+        {/* <p className="text-red-500">Doctor not found.</p> */}
+       
+       <Link href="/doctors">
+       <button
+        
+          className="mt-4 flex items-center gap-2 px-5 py-3 bg-gradient-to-r from-blue-500 to-indigo-600 text-white rounded-full shadow-md hover:from-blue-600 hover:to-indigo-700 transition-transform transform hover:scale-105"
+        >
+          <svg className="w-5 h-5" fill="none" stroke="currentColor" strokeWidth="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" d="M15 19l-7-7 7-7"></path></svg>
+          Back
+        </button>
+       </Link>
+    
+
+       
+      </div>
       <div className="card lg:card-side bg-base-100 shadow-xl">
         {doctor.photo && (
           <div className="flex justify-center items-center p-4">
